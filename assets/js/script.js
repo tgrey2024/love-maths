2746916cb5f7325ea6447a05091295f052bd7308
Tripta Grey <tripta.grey@gmail.com>
2024-11-22 11:51:50 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button"); // get all the buttons in the HTML
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+
 function runGame(){
 
 }
@@ -23,5 +41,5 @@ function displayAdditionQuestion() {
 }
 
 function displaySubtractQuestion() {
-    
+
 }
