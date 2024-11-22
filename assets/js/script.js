6a6facc9a2b67c006e5873a360df9b5092bb0d83
Tripta Grey <tripta.grey@gmail.com>
2024-11-22 21:03:19 +0000

    Add Enter key functionality to answer box and update page title

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+    document.getElementById('answer-box').addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
     runGame("addition");
 });
 
@@ -22,6 +28,8 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 
 function runGame(gameType) {
+    document.getElementById('answer-box').value = ""; // clear the value of the answer box
+    document.getElementById('answer-box').focus(); // focus the cursor on the answer box
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
